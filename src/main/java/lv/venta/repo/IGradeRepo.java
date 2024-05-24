76d175a28acf8c922d74b3abfc28bc9a0f8bd275
martinsauzins <s22stuledua@venta.lv>
2024-05-24 16:13:02 +0300

    24.05.24

--- src/main/java/lv/venta/repo/IGradeRepo.java
+++ src/main/java/lv/venta/repo/IGradeRepo.java
@@ -1,9 +1,21 @@
 package lv.venta.repo;
 
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.model.Grade;
 
 public interface IGradeRepo extends CrudRepository<Grade, Long> {
+
+	//public abstract by default
+	ArrayList<Grade> findByGrvalueLessThan(int i);
+	//public abstract by default
+	ArrayList<Grade> findByStudentIds(long id);
+	
+	//public abstract by default
+	@Query(nativeQuery = true, value = "select avg(grvalue) from grade_table where idc=(?1);")
+	float calculateAVGGrade_MyFunction(long id);
 
 }
